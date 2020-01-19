d811bb66c0a87cd25aa301c29eec445a060c70ea
LPMartinez <lpmartinez1109@gmail.com>
2020-01-19 10:36:27 -0600

    Created function for add manager

---
+++ app.js
@@ -0,0 +1,22 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require ("fs");
+
+const outputPath = path.resolve(__dirname, "output", "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
+const idArray = [];
+function appMenu() {
+
+    function createManager(){
+        console.log("World's Greatest Boss!");
+        
+        inquirer
+        prompt
+    }
+}
