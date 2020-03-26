9c6b5d131558239e9c518fa9d65b877b84bc54f0
LPMartinez <lpmartinez1109@gmail.com>
2020-03-26 12:46:00 -0500

    Files edited to fix html rendering issue. Team.html file should start display some of the current information, needs more fix

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ function appMenu() {
         if(!fs.existsSync(OUTPUT_DIR)){
             fs.mkdirSync(OUTPUT_DIR)
         }
-        fs.writeFileSync(outputPath, render(teamMemebers), "utf-8")
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
     }
 
     createManager();
